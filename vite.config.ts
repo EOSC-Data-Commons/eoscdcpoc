e8596e8a13d067d2791c66f5805826e425a94a12
Ritwik Shanker <ritwikshanker@yahoo.in>
2025-07-22 20:43:14 +0200

    Updated Vite and TypeScript configurations:
    - Switched to `@vitejs/plugin-react-swc` for improved performance.
    - Added Tailwind CSS plugin.
    - Configured server settings (host and port) and output directory.
    - Introduced path aliases for cleaner imports.
    - Refined TypeScript settings with module resolution, path mapping, and new include/exclude rules.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+    server: {
+        host: "::",
+        port: 8080,
+    },
+    build: {
+        outDir: "dist/spa",
+    },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@shared": path.resolve(__dirname, "./shared"),
+        },
+    },
+}));
