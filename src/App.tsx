276028ab70cce3137434957a312d1df3ef69ebb9
Ritwik Shanker <ritwikshanker@yahoo.in>
2025-10-31 15:34:10 +0100

    Refactor Matomo integration by moving tracking logic to a dedicated component

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,13 @@ import {LandingPage} from "./pages/LandingPage";
 import {SearchPage} from "./pages/SearchPage";
 import NotFound from "./pages/NotFound";
 import {ErrorBoundary} from "./components/ErrorBoundary";
-
-declare global {
-    interface Window {
-        _mtm: Record<string, unknown>[];
-    }
-}
+import MatomoTracker from "./components/MatomoTracker";
 
 export default function App() {
-    React.useEffect(() => {
-        const matomoUrl = import.meta.env.VITE_MATOMO_CONTAINER_URL;
-
-        if (!matomoUrl) {
-            console.warn('VITE_MATOMO_CONTAINER_URL is not defined in environment variables');
-            return;
-        }
-
-        window._mtm = window._mtm || [];
-        window._mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-        const d = document;
-        const g = d.createElement('script');
-        const s = d.getElementsByTagName('script')[0];
-        g.async = true;
-        g.src = matomoUrl;
-        s.parentNode!.insertBefore(g, s);
-    }, []);
 
     return (
         <BrowserRouter>
+            <MatomoTracker />
             <ErrorBoundary>
                 <Routes>
                     <Route path="/" element={<LandingPage/>}/>
---
+++ src/components/MatomoTracker.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+
+declare global {
+    interface Window {
+        _paq: (string[] | number[] | ((this: void) => void))[];
+    }
+}
+
+const MatomoTracker = () => {
+  useEffect(() => {
+    window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    window._paq.push(['trackPageView']);
+    window._paq.push(['enableLinkTracking']);
+    (function() {
+      const u="https://egi.matomo.cloud/";
+      window._paq.push(['setTrackerUrl', u+'matomo.php']);
+      window._paq.push(['setSiteId', '10']);
+      const d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src='https://cdn.matomo.cloud/egi.matomo.cloud/matomo.js';
+      if(s && s.parentNode) {
+        s.parentNode.insertBefore(g,s);
+      }
+    })();
+  }, []);
+
+  return null;
+};
+
+export default MatomoTracker;
+
