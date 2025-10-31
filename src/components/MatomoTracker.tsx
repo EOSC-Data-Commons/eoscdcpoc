333b94bf1dc66d4b36857efb90bd9f46ab075f06
Ritwik Shanker <ritwikshanker@yahoo.in>
2025-10-31 16:13:11 +0100

    Enhance MatomoTracker to track page views on route changes using `useLocation`.

--- src/components/MatomoTracker.tsx
+++ src/components/MatomoTracker.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 
 declare global {
     interface Window {
@@ -7,25 +8,35 @@ declare global {
 }
 
 const MatomoTracker = () => {
-  useEffect(() => {
-    window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    window._paq.push(['trackPageView']);
-    window._paq.push(['enableLinkTracking']);
-    (function() {
-      const u="https://egi.matomo.cloud/";
-      window._paq.push(['setTrackerUrl', u+'matomo.php']);
-      window._paq.push(['setSiteId', '10']);
-      const d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.async=true; g.src='https://cdn.matomo.cloud/egi.matomo.cloud/matomo.js';
-      if(s && s.parentNode) {
-        s.parentNode.insertBefore(g,s);
-      }
-    })();
-  }, []);
+    const location = useLocation();
 
-  return null;
+    useEffect(() => {
+        window._paq = window._paq || [];
+        (function () {
+            const u = "https://egi.matomo.cloud/";
+            window._paq.push(['setTrackerUrl', u + 'matomo.php']);
+            window._paq.push(['setSiteId', '10']);
+            const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+            g.async = true;
+            g.src = 'https://cdn.matomo.cloud/egi.matomo.cloud/matomo.js';
+            if (s && s.parentNode) {
+                s.parentNode.insertBefore(g, s);
+            }
+        })();
+    }, []);
+
+    // This effect runs on every route change to track page views.
+    useEffect(() => {
+        if (window._paq) {
+            window._paq.push(['setCustomUrl', location.pathname + location.search]);
+            window._paq.push(['trackPageView']);
+            window._paq.push(['enableLinkTracking']);
+        }
+    }, [location]);
+
+    return null;
 };
 
 export default MatomoTracker;
+
 
